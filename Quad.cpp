5c43fb78c7faa7d2b6309e932542c8f18e8924ae
JackiEwens <55686071+JackiEwens@users.noreply.github.com>
2019-12-30 15:31:04 +0300

    Add files via upload

---
+++ Quad.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+#include <math.h>
+#include <assert.h>
+#include <string>
+#include "QuadForm.h"
+
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	QuadForm obj;
+	system("PAUSE");
+	return 0;
+}
---
+++ QuadForm.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+#include <cmath>
+#include <sstream>
+
+using namespace std;
+
+class QuadForm	
+{
+public:
+	QuadForm() : mas(new string[2]) {};
+	~QuadForm() {
+		delete[] mas;
+	};
+	string* Quadform(string a, string b, string c);
+private:
+	string* mas;
+};
